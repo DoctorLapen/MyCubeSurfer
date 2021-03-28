95a9943e99389f89ebfaf3d93b6768262e915265
DoctorLapen <kostia1272@gmail.com>
2021-03-29 01:32:05 +0300

    Add finish handling

---
+++ Assets/Scripts/FinishHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyCubeSurfer
+{
+    public class FinishHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private string _playerTag;
+
+        [SerializeField]
+        private RectTransform _levelPassedScreen;
+
+        
+
+        
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.CompareTag(_playerTag))
+            {
+                Time.timeScale = 0;
+                _levelPassedScreen.gameObject.SetActive(true);
+            }
+        }
+    }
+}
