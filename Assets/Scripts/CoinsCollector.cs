7d08ec8b59f32c35898dea30816572609a94483b
DoctorLapen <kostia1272@gmail.com>
2021-03-29 00:31:09 +0300

    Add coins collecting

---
+++ Assets/Scripts/CoinsCollector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyCubeSurfer
+{
+    public class CoinsCollector : MonoBehaviour
+    {
+        [SerializeField]
+        private string _coinTag;
+
+        [SerializeField]
+        private CoinsView _coinsView;
+
+        
+
+        private int _coinsAmount = 0;
+        
+
+        
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.CompareTag(_coinTag))
+            {
+                _coinsAmount++;
+                _coinsView.UpdateCoins(_coinsAmount);
+                Destroy(coll.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CoinsView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyCubeSurfer
+{
+    public class CoinsView : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _coinsText;
+
+        [SerializeField]
+        private string _startText;
+
+        
+
+        public void UpdateCoins(int coins)
+        {
+            _coinsText.text = coins.ToString();
+        }
+
+
+    }
+}
